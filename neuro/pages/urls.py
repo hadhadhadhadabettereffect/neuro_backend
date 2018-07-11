1f536ad11eadfb783d8b3b493f19a168028ec666
Eric Birkeland <ericjbirkeland@gmail.com>
2018-07-11 13:49:09 -0400

    fixed url function

--- neuro/pages/urls.py
+++ neuro/pages/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     # Notice the URL has been named
     path('', views.HomePageView.as_view(), name='home'),
-    path('collection', views.get_collection_html.as_view(), name='collection')
+    path('collection', views.get_collection_html, name='collection')
 ]
