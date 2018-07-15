3ec39a6e3f9c3e9713a1c3da7fdef85756012356
Eric Birkeland <ericjbirkeland@gmail.com>
2018-07-15 19:04:42 -0400

    serving templates together

--- neuro/pages/views.py
+++ neuro/pages/views.py
@@ -1,18 +1,32 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
 
+from neuro.contact.models import Contact
 from neuro.products.models import Product
 from neuro.work.models import Project
 
-class HomePageView(TemplateView):
-    template_name = "index.html"
+
+context = {
+    'agency_offsets_1': 1,
+    'agency_offsets_2': 2,
+    'agency_offsets_3': 2 + Project.objects.count(),
+    'collection_offsets_1': 2,
+    'collection_offsets_2': 3,
+    'collection_offsets_3': 4,
+    'product_list': Product.objects.all(),
+    'project_list': Project.objects.all(),
+    'contact':  Contact.load().__dict__,
+}
+
+
+def get_home(request):
+    a_offsets = [0, 1, 2, 2 + Project.objects.count()]
+
+    return render(request, 'index.html', context)
+
 
 def get_agency_html(request):
-    return render(request, "agency.html", {
-        'project_list': Project.objects.all()
-    })
+    return render(request, "agency.html", context)
+
 
 def get_collection_html(request):
-    return render(request, "collection.html", {
-        'product_list': Product.objects.all()
-    })
+    return render(request, "collection.html", context)
--- neuro/settings.py
+++ neuro/settings.py
@@ -142,13 +142,12 @@ class Common(Configuration):
 
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
     STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    MEDIA_URL = '/media/'
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'neuro/static')]
 
 class Dev(Common):
     DEBUG = True
